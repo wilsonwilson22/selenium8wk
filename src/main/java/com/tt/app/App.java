0dd5d3752f7932296724aba0579281dceb662cef
John Wilson <john1234wilson5678@gmail.com>
2017-12-09 00:25:40 -0400

    first commit

---
+++ src/main/java/com/tt/app/App.java
@@ -0,0 +1,13 @@
+package com.tt.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
